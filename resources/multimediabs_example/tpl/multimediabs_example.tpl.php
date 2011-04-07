d505b5a841dd14a517e86229bd4d989cb8c1f99a
Dominique De Cooman <dominique@dominiquedecooman.com>
2011-04-07 11:29:04 +0200

    remove example

--- resources/multimediabs_example/tpl/multimediabs_example.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $Id: multimediabs_example.tpl.php Exp $
-?>
-
-<div id="multimediabs-message">
-  <?php print t('Message : @message', array('@message' => $message)) ?>
-</div>
--- resources/multimediabs_example/tpl/multimediabs_example_import_page.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $Id: multimediabs_example.tpl.php Exp $
-?>
-
-<div id="multimediabs-message">
-  <?php print '<h3>'.$title.'</h3>'; ?>
-  <?php print '<div>'.$body.'</div>'; ?>
-  <?php print '<div>'.$link.'</div>'; ?>
-</div>
